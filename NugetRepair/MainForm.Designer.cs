3319eb27e4b15b225715d3a8a27c089a5cff8a38
yangguanghui <yangguanghui@beisen.com>
2021-02-09 15:50:00 +0800

    支持.csproj修复

---
+++ NugetRepair/MainForm.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace NugetRepair
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btnOpen1 = new System.Windows.Forms.Button();
+            this.btnOpen2 = new System.Windows.Forms.Button();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.btnOpenDir1 = new System.Windows.Forms.Button();
+            this.btnOpenDir2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 53);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(460, 165);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(6, 53);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(460, 175);
+            this.textBox2.TabIndex = 1;
+            // 
+            // btnOpen1
+            // 
+            this.btnOpen1.Location = new System.Drawing.Point(6, 22);
+            this.btnOpen1.Name = "btnOpen1";
+            this.btnOpen1.Size = new System.Drawing.Size(75, 25);
+            this.btnOpen1.TabIndex = 2;
+            this.btnOpen1.Text = "文件";
+            this.btnOpen1.UseVisualStyleBackColor = true;
+            this.btnOpen1.Click += new System.EventHandler(this.btnOpen1_Click);
+            // 
+            // btnOpen2
+            // 
+            this.btnOpen2.Location = new System.Drawing.Point(6, 22);
+            this.btnOpen2.Name = "btnOpen2";
+            this.btnOpen2.Size = new System.Drawing.Size(75, 25);
+            this.btnOpen2.TabIndex = 3;
+            this.btnOpen2.Text = "文件";
+            this.btnOpen2.UseVisualStyleBackColor = true;
+            this.btnOpen2.Click += new System.EventHandler(this.btnOpen2_Click);
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(12, 486);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(75, 25);
+            this.btnOk.TabIndex = 4;
+            this.btnOk.Text = "确定";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // btnOpenDir1
+            // 
+            this.btnOpenDir1.Location = new System.Drawing.Point(87, 22);
+            this.btnOpenDir1.Name = "btnOpenDir1";
+            this.btnOpenDir1.Size = new System.Drawing.Size(75, 25);
+            this.btnOpenDir1.TabIndex = 5;
+            this.btnOpenDir1.Text = "目录";
+            this.btnOpenDir1.UseVisualStyleBackColor = true;
+            this.btnOpenDir1.Click += new System.EventHandler(this.btnOpenDir1_Click);
+            // 
+            // btnOpenDir2
+            // 
+            this.btnOpenDir2.Location = new System.Drawing.Point(87, 22);
+            this.btnOpenDir2.Name = "btnOpenDir2";
+            this.btnOpenDir2.Size = new System.Drawing.Size(75, 25);
+            this.btnOpenDir2.TabIndex = 6;
+            this.btnOpenDir2.Text = "目录";
+            this.btnOpenDir2.UseVisualStyleBackColor = true;
+            this.btnOpenDir2.Click += new System.EventHandler(this.btnOpenDir2_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnOpenDir1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.btnOpen1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(474, 231);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "来源";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnOpen2);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Controls.Add(this.btnOpenDir2);
+            this.groupBox2.Location = new System.Drawing.Point(12, 249);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(474, 231);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "目标";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(499, 521);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnOk);
+            this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnOpen1;
+        private System.Windows.Forms.Button btnOpen2;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Button btnOpenDir1;
+        private System.Windows.Forms.Button btnOpenDir2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
---
+++ NugetRepair/MainForm.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace NugetRepair
+{
+    public partial class MainForm : Form
+    {
+        const string packagesConfig = "packages.config";
+        const string csprojExtension = ".csproj";
+        string lastFolderBrowserdir = string.Empty;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpen1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.RestoreDirectory = true;
+            ofd.Filter = "C#项目文件|*.csproj|程序包文件|packages.config|所有文件|*.*";
+            ofd.Multiselect = true;
+            if(ofd.ShowDialog()==DialogResult.OK)
+            {
+                string[] paths = ofd.FileNames;
+                foreach(string path in paths)
+                {
+                    textBox1.AppendText(path + Environment.NewLine);
+                }
+            }
+        }
+
+        private void btnOpen2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.RestoreDirectory = true;
+            ofd.Filter = "C#项目文件|*.csproj|程序包文件|packages.config|所有文件|*.*";
+            ofd.Multiselect = true;
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                string[] paths = ofd.FileNames;
+                foreach (string path in paths)
+                {
+                    textBox2.AppendText(path + Environment.NewLine);
+                }
+            }
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            string[] paths1 = Regex.Split(textBox1.Text.TrimEnd(Environment.NewLine.ToArray()), Environment.NewLine);
+            foreach (string path1 in paths1)
+            {
+                if (!File.Exists(path1))
+                {
+                    MessageBox.Show($"源文件 {path1} 不存在！");
+                    return;
+                }
+            }
+
+            string[] paths2 = Regex.Split(textBox2.Text.TrimEnd(Environment.NewLine.ToArray()), Environment.NewLine);
+            foreach (string path2 in paths2)
+            {
+                if (!File.Exists(path2))
+                {
+                    MessageBox.Show($"目标文件 {path2} 不存在！");
+                    return;
+                }
+            }
+
+            var packagesConfigPaths = new List<string>();
+            var csprojPaths = new List<string>();
+            foreach (var path in paths1)
+            {
+                var fileName = Path.GetFileName(path);
+                if(fileName.ToLower() == packagesConfig)
+                {
+                    packagesConfigPaths.Add(path);
+                }
+                else if(Path.GetExtension(fileName).ToLower() == csprojExtension)
+                {
+                    csprojPaths.Add(path);
+                }
+            }
+
+            Dictionary<string, XElement> packageDic = ReadPackagesConfig(packagesConfigPaths);
+            Dictionary<string, XElement> referenceDic = ReadCsproj(csprojPaths);
+
+
+            //更新目标
+            foreach (string path2 in paths2)
+            {
+                if (!File.Exists(path2))
+                {
+                    continue;
+                }
+
+                var fileName = Path.GetFileName(path2);
+                if (fileName.ToLower() == packagesConfig)
+                { //packages.config
+                    RepairPackageConfig(path2, packageDic);
+                }
+                else if (Path.GetExtension(fileName).ToLower() == csprojExtension)
+                { //.csproj
+                    RepairCsprojConfig(path2, referenceDic);
+                }
+            }
+
+            MessageBox.Show("修复成功！");
+        }
+
+        /// <summary>
+        /// 修复packages.config
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="packageDic"></param>
+        private void RepairPackageConfig(string path, Dictionary<string, XElement> packageDic)
+        {
+            XDocument xd = XDocument.Load(path);
+            var packages = xd.Descendants("packages").FirstOrDefault();
+            var packageList = packages.Descendants("package");
+            for (int i = 0; i < packageList.Count(); i++)
+            {
+                string id = packageList.ElementAt(i).Attribute("id").Value;
+                if (packageDic.TryGetValue(id, out XElement tn))
+                {
+                    packageList.ElementAt(i).ReplaceWith(tn);
+                    packageDic.Remove(id); //移除掉更新的
+                }
+            }
+
+            packages.Add(packageDic.Values); //添加没有的
+
+            xd.Save(path);
+        }
+
+        /// <summary>
+        /// 修复.csproj文件
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="csprojDic"></param>
+        private void RepairCsprojConfig(string path, Dictionary<string, XElement> csprojDic)
+        {
+            XDocument xd = XDocument.Load(path);
+            var references = xd.Descendants().Where(x => x.Name.LocalName == "Reference");
+            var itemGroup = references.First().Parent;
+            for (int i = 0; i < references.Count(); i++)
+            {
+                var reference = references.ElementAt(i);
+                string include = reference.Attribute("Include").Value;
+                string id = include.Split(',').First().Trim();
+                if (csprojDic.TryGetValue(id, out XElement tn))
+                {
+                    references.ElementAt(i).ReplaceWith(tn);
+                    csprojDic.Remove(id); //移除掉更新的
+                }
+            }
+
+            itemGroup.Add(csprojDic.Values); //添加没有的
+
+            xd.Save(path);
+        }
+
+        private void btnOpenDir1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (Directory.Exists(lastFolderBrowserdir)) fbd.SelectedPath = lastFolderBrowserdir;
+            if(fbd.ShowDialog() == DialogResult.OK)
+            {
+                string dir = fbd.SelectedPath;
+                DirectoryInfo root = new DirectoryInfo(dir);
+                FileInfo[] files = root.GetFiles();
+                foreach(var file in files)
+                {
+                    if (file.Name.ToLower() == packagesConfig || Path.GetExtension(file.Name).ToLower() == csprojExtension)
+                    {
+                        textBox1.AppendText(file.FullName);
+                    }
+                }
+                lastFolderBrowserdir = dir;
+            }
+        }
+
+        private void btnOpenDir2_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (Directory.Exists(lastFolderBrowserdir)) fbd.SelectedPath = lastFolderBrowserdir;
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                string dir = fbd.SelectedPath;
+                DirectoryInfo root = new DirectoryInfo(dir);
+                FileInfo[] files = root.GetFiles();
+                foreach (var file in files)
+                {
+                    if (file.Name.ToLower() == packagesConfig || Path.GetExtension(file.Name).ToLower() == csprojExtension)
+                    {
+                        textBox2.AppendText(file.FullName);
+                    }
+                }
+                lastFolderBrowserdir = dir;
+            }
+        }
+
+        /// <summary>
+        /// 读取packages.config
+        /// </summary>
+        /// <param name="paths"></param>
+        /// <returns></returns>
+        private Dictionary<string, XElement> ReadPackagesConfig(IEnumerable<string> paths)
+        {
+            Dictionary<string, XElement> nugetDic = new Dictionary<string, XElement>();
+            //读取源
+            foreach (string path in paths)
+            {
+                if (!File.Exists(path))
+                {
+                    continue;
+                }
+                XDocument xd = XDocument.Load(path);
+                XElement packages = xd.Descendants("packages").FirstOrDefault();
+                List<XElement> packageList = packages.Descendants("package").ToList();
+                foreach (XElement package in packageList)
+                {
+                    string id = package.Attribute("id").Value;
+                    if (!nugetDic.ContainsKey(id)) nugetDic.Add(id, package);
+                }
+            }
+
+            return nugetDic;
+        }
+
+        /// <summary>
+        /// 读取***.csproj
+        /// </summary>
+        /// <param name="paths"></param>
+        /// <returns></returns>
+        private Dictionary<string, XElement> ReadCsproj(IEnumerable<string> paths)
+        {
+            Dictionary<string, XElement> nugetDic = new Dictionary<string, XElement>();
+            foreach (string path in paths)
+            {
+                if (!File.Exists(path))
+                {
+                    continue;
+                }
+                XDocument xd = XDocument.Load(path);
+                var references = xd.Descendants().Where(x=>x.Name.LocalName== "Reference");
+                foreach (XElement reference in references)
+                {
+                    string include = reference.Attribute("Include").Value;
+                    string id = include.Split(',').First().Trim();
+                    if (!nugetDic.ContainsKey(id)) nugetDic.Add(id, reference);
+                }
+            }
+            return nugetDic;
+        }
+    }
+}
